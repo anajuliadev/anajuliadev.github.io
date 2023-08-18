bfcd195cc7d62868f3ddab12eefd0315b952110b
Ana Júlia <anaju.silvaalves@gmail.com>
2023-08-17 23:37:30 -0300

    atualizacoes

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const pastinhas = document.querySelectorAll('.pastinha');
 pastinhas.forEach(pasta => {
     pasta.addEventListener('click', function abrirPastinha(event){
         console.log(event);
-        pasta.classList.add('pretina');
+ 
+
+        if (pasta.classList.contains('aberto')) {
+            pasta.classList.add('fechado')
+            pasta.classList.remove('aberto')
+        } else {
+            pasta.classList.add('aberto')
+            pasta.classList.remove('fechado')
+        }
     });
+    
 });
 
+//se pastinha tiver a classe 'aberto' , add a classe 'fechado'
+// se tiver a calsse 'fechado' , add 'aberto' 
